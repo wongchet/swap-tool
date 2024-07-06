d717df4cc7e796d0421ef6918527fb3193be53b3
RongzhuoHuang <rongzhuo.huang@shopee.com>
2024-07-06 15:24:45 +0800

    add

--- swap/src/main/java/org/tron/swap/Main.java
+++ swap/src/main/java/org/tron/swap/Main.java
@@ -1,11 +1,18 @@
 package org.tron.swap;
 
+import org.tron.trident.abi.FunctionReturnDecoder;
+import org.tron.trident.abi.TypeReference;
+import org.tron.trident.abi.datatypes.Function;
+import org.tron.trident.abi.datatypes.Utf8String;
 import org.tron.trident.core.ApiWrapper;
 import org.tron.trident.core.contract.Contract;
 import org.tron.trident.core.contract.Trc20Contract;
 import org.tron.trident.proto.Response.*;
+import org.tron.trident.utils.Numeric;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author: Chet Wong
@@ -25,17 +32,35 @@ public class Main {
     );
 
     public static void main(String[] args) throws Exception {
+        // acct
         long bal = wrapper.getAccountBalance("TQooBX9o8iSSprLWW96YShBogx7Uwisuim");
         System.out.println(bal);
 
+        // transaction
         TransactionSignWeight transaction = wrapper.getTransactionSignWeight(
                 wrapper.getTransactionById("fd15cc89a6533faba4673f151f1f443f5939ad2e8c9800e767d44e99148f0ad3"));
         System.out.println(transaction);
 
+        // trc20
         Contract contract = wrapper.getContract("TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t");
         Trc20Contract token = new Trc20Contract(contract, "TQooBX9o8iSSprLWW96YShBogx7Uwisuim", wrapper);
         BigInteger supply = token.totalSupply();
         System.out.println(supply);
+
+        // call func
+        Function name = new Function(
+                "name",
+                Collections.emptyList(),
+                Arrays.asList(new TypeReference<Utf8String>() {})
+        );
+        TransactionExtention txnExt = wrapper.constantCall(
+                "TQooBX9o8iSSprLWW96YShBogx7Uwisuim",
+                "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+                name
+        );
+        String result = Numeric.toHexString(txnExt.getConstantResult(0).toByteArray());
+        String value = (String) FunctionReturnDecoder.decode(result, name.getOutputParameters()).get(0).getValue();
+        System.out.println(value);
     }
 
 
